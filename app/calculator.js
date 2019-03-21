581ad4d4b260fa97d51a4470578c6267919921c5
Jackie Carter <jacqueline.carter@hotmail.co.uk>
2019-03-21 16:55:23 +0000

    Updated during coding exercises Day2

--- app/calculator.js
+++ app/calculator.js
@@ -1,7 +1,43 @@
 function add(a, b) {
     return a + b;
 }
-
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function isOdd(a) {
+    if (a % 2 == 1) 
+        return true;
+     else 
+        return false; 
+}
+function isEven(a) {
+    if (a % 2 == 1) 
+        return false;
+     else 
+        return true; 
+}
+function divisibleBy(a, b) {
+    if (a % b == 0) 
+        return true;
+     else 
+        return false; 
+}
+function square(a) {
+    return a * a;
+}
+function square2(a) {
+    return Math.pow(a,2);
+}
 module.exports = {
-    add
+    add,
+    subtract,
+    multiply,
+    isOdd,
+    isEven,
+    divisibleBy,
+    square,
+    square2
 };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -2,4 +2,29 @@ const calculator = require('../app/calculator');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
-});
+})
+  test('subtract 2 -1 to equal 1', () => {
+    expect(calculator.subtract(2, 1)).toBe(1);
+  })   
+    test('multiply 2 * 3 to equal 6', () => {
+      expect(calculator.multiply(2, 3)).toBe(6);
+  })
+  test('isOdd 35 to equal odd', () => {
+    expect(calculator.isOdd(35)).toBe(true);
+})
+test('isEven 35 to equal false', () => {
+  expect(calculator.isEven(35)).toBe(false);
+})
+test('isEven 35 to equal false', () => {
+  expect(calculator.isEven(35)).toBe(false);
+})
+test('divisibleBy 35 / 4 to equal false', () => {
+  expect(calculator.divisibleBy(35, 4)).toBe(false);
+})
+test('square 4 equal 16', () => {
+  expect(calculator.square(4)).toBe(16);
+})
+test('square 4 equal 16', () => {
+  expect(calculator.square2(4)).toBe(16);
+})
+;
